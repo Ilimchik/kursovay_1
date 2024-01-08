d3486c77be6adbaf2c806fa3325be1bdaa6a2b2e
Ilimchik <ilimbork@gmail.com>
2024-01-08 14:05:45 +0600

    ch css

--- js/core.js
+++ js/core.js
@@ -2,20 +2,25 @@ const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-  
+    autoplay: {
+        delay: 2000,
+    },
+
     // If we need pagination
     pagination: {
-      el: '.swiper-pagination',
+        el: '.swiper-pagination',
+        clickable: true,
     },
-  
+
     // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
-  
+
     // And if we need scrollbar
     scrollbar: {
-      el: '.swiper-scrollbar',
+        el: '.swiper-scrollbar',
     },
-  });
+    // slidesPerGroup: 3,
+});
